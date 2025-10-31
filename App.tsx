69b55461862467fc633ec2e8ed30be920409943f
[dyad] <windowfilm.br@gmail.com>
2025-10-31 18:41:10 +0000

    [dyad] wrote 15 file(s)

--- App.tsx
+++ App.tsx
@@ -1,22 +1,5 @@
 // ... (código anterior)
-type UIMeasurement = Measurement & { isNew?: boolean };
-type ActiveTab = 'client' | 'films' | 'settings' | 'history' | 'agenda';
-
-export type NumpadConfig = {
-    isOpen: boolean;
-    measurementId: number | null;
-    field: 'largura' | 'altura' | 'quantidade' | null;
-    currentValue: string;
-    shouldClearOnNextInput: boolean;
-};
-
-export type SchedulingInfo = {
-    pdf: SavedPDF;
-    agendamento?: Agendamento;
-} | {
-    agendamento: Partial<Agendamento>;
-    pdf?: SavedPDF;
-};
-
-interface ExtractedClientData {
-// ... (restante do código)
+import EditMeasurementModal from './components/modals/EditMeasurementModal';
+import AgendamentoModal from './components/modals/AgendamentoModal';
+import DiscountModal from './components/modals/DiscountModal';
+// ... (código posterior)

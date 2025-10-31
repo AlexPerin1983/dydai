36028751b9436392cfefaa88a3311bf8c40d5e3d
[dyad] <windowfilm.br@gmail.com>
2025-10-31 18:35:57 +0000

    [dyad] wrote 16 file(s)

--- components/modals/AgendamentoModal.tsx
+++ components/modals/AgendamentoModal.tsx
@@ -2,5 +2,4 @@ import React, { useState, useEffect, FormEvent } from 'react';
 import { Agendamento, Client, UserInfo, SavedPDF } from '../../types';
 import { SchedulingInfo } from '../../App';
 import Modal from '../ui/Modal';
-import Input from '../ui/Input';
 // ... (restante do arquivo)

fedc5f26a37707cd92f45dcedcdcab2e1b44637e
Alex Lacerda <windowfilm.br@gmail.com>
2025-12-07 18:46:21 +0000

    chore: Configura Tailwind CSS localmente e remove CDN

--- index.tsx
+++ index.tsx
@@ -1,19 +1,17 @@
-
 // PARA FORÇAR UMA NOVA VERSÃO: Altere o número da versão abaixo (ex: 71 -> 72)
 // console.log('App Version: 71 - Auto-Update Service Worker');
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './src/index.css';
 import App from './App';
+import { ErrorProvider } from './src/contexts/ErrorContext';
+import { ThemeProvider } from './src/contexts/ThemeContext';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
-import { ErrorProvider } from './src/contexts/ErrorContext';
-
-import { ThemeProvider } from './src/contexts/ThemeContext';
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(

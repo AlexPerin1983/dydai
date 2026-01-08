f5bd30b5e2c84e761f18d22a43c36aa42de90901
Alex Lacerda <windowfilm.br@gmail.com>
2026-01-08 06:38:46 +0000

    fix: atualiza credenciais do Supabase para projeto correto (ydupkaqexrdnusumqmoc)

--- services/supabaseConfig.ts
+++ services/supabaseConfig.ts
@@ -1,7 +1,7 @@
 // Configuração do Supabase para produção
-// Credenciais do projeto peliculas-br-testes
+// Credenciais do projeto pelicula para janelas (ydupkaqexrdnusumqmoc)
 
 export const supabaseConfig = {
-    url: 'https://avlefzsipbqvollukgyt.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2bGVmenNpcGJxdm9sbHVrZ3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3Nzc0MjUsImV4cCI6MjA4MjM1MzQyNX0.mXiqnxe9reQNwuAjZ6yFfm1AR1Qcdib3EjXCaG9EonM'
+    url: 'https://ydupkaqexrdnusumqmoc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkdXBrYXFleHJkbnVzdW1xbW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMzU0MjQsImV4cCI6MjA3OTYxMTQyNH0.UDgI0FMt4gI8vxW2N9HjeaZaeEobGoXD-Q6zhB23utc'
 };
